44b2d01d86712c48a46ac9dab2261d2e6935d67a
Sherifdeen Ishola <isholasherifdeen@gmail.com>
2022-12-06 12:49:01 +0100

    .env

--- myApp.js
+++ myApp.js
@@ -1,13 +1,22 @@
+require("dotenv").config();
 let express = require("express");
 let app = express();
 
 app.use("/public", express.static(`${__dirname}/public`));
 
+const msgString = "Hello json";
+
+if (process.env.MESSAGE_STYLE === "uppercase") {
+  response = msgString.toUpperCase();
+} else {
+  response = msgString;
+}
+
 app.get("/", (req, res) => {
   res.sendFile(`${__dirname}/views/index.html`);
 });
 app.get("/json", (req, res) => {
-  res.json({ message: "Hello json" });
+  res.json({ message: response });
 });
 
 module.exports = app;
