5c8011f918a130b5ce1163f43c773e064babc2fb
Sherifdeen Ishola <isholasherifdeen@gmail.com>
2022-12-07 16:40:25 +0100

    ......................

--- myApp.js
+++ myApp.js
@@ -4,20 +4,16 @@ let app = express();
 
 app.use("/public", express.static(`${__dirname}/public`));
 
-const msgString = "Hello json";
-
-if (process.env.MESSAGE_STYLE === "uppercase") {
-  response = msgString.toUpperCase();
-} else {
-  response = msgString;
-}
-
 app.get("/", (req, res) => {
   res.sendFile(`${__dirname}/views/index.html`);
 });
 app.get("/json", (req, res) => {
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    response = "Hello json".toUpperCase();
+  } else {
+    response = "Hello json";
+  }
   res.json({ message: response });
 });
 
 module.exports = app;
- 
