f780f307d703e3356ccd0dac0775b0f68a98ce7f
Sherifdeen Ishola <isholasherifdeen@gmail.com>
2022-12-08 08:43:25 +0100

    query

--- myApp.js
+++ myApp.js
@@ -32,5 +32,8 @@ app.get(
 app.get("/:word/echo", (req, res) => {
   res.json({ echo: req.params.word });
 });
+app.get("/name", (req, res) => {
+  res.json({ name: `${req.query.first} ${req.query.last}` });
+});
 
 module.exports = app;
